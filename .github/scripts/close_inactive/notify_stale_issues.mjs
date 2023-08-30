6562663513560bddf8e816ceabc5d6b64b7c1a3b
Charly López <charly@keyko.io>
2023-08-30 12:25:42 +0200

    Prepare for production

--- .github/scripts/close_inactive/notify_stale_issues.mjs
+++ .github/scripts/close_inactive/notify_stale_issues.mjs
@@ -2,7 +2,7 @@
 
 import { Octokit } from "@octokit/rest";
 import fetch from "node-fetch";
-import { getLastComment } from "./utils.mjs";
+import { getLastComment, checkThrottling } from "./utils.mjs";
 
 const DAYS_TO_WAIT = 10;
 const owner = process.env.GITHUB_REPOSITORY.split('/')[0];
@@ -15,7 +15,11 @@ const octokit = new Octokit({
   }
 });
 
+/**
+ * This function checks the rate limit status and sleeps if the remaining requests are below the limit.
+ */
 async function checkAndCommentOnIssues() {
+  await checkThrottling(octokit);
 
   let issues = await octokit.paginate(octokit.issues.listForRepo, {
     owner,
@@ -26,44 +30,39 @@ async function checkAndCommentOnIssues() {
 
   const dateThreshold = new Date();
   dateThreshold.setDate(dateThreshold.getDate() - DAYS_TO_WAIT);
-  
-  Promise.allSettled(
-    issues.map(
-      async (issue) => new Promise(
-        async (resolve, reject) => {
-          const lastComment = await getLastComment(octokit, owner, repo, issue.number, true);
-          if (lastComment && 
-            lastComment.user.login == 'github-actions[bot]' && 
-            lastComment.body.includes("Commented by Stale Bot.")
-          ) return resolve(); //Do not comment again if already commented by bot
-          
-          const updatedAt = lastComment 
-            ? new Date(lastComment.created_at) 
-            : new Date(issue.created_at);
 
-          // Let's calculate the difference between the two dates
-          const diffTime = dateThreshold - updatedAt;
-          const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  for (let issue of issues) {
+    await checkThrottling(octokit);
 
-          if (diffDays > 0) {
-            await octokit.issues.createComment({
-              owner,
-              repo,
-              issue_number: issue.number,
-              body: `This application has not seen any responses in the last 10 days. This issue will be marked with Stale label and will be closed in 4 days. Comment if you want to keep this application open.
+    const lastComment = await getLastComment(octokit, owner, repo, issue.number, true);
+    if (lastComment && 
+      lastComment.user.login == 'github-actions[bot]' && 
+      lastComment.body.includes("Commented by Stale Bot.")
+    ) continue; //Do not comment again if already commented by bot
+    
+    const updatedAt = lastComment 
+      ? new Date(lastComment.created_at) 
+      : new Date(issue.created_at);
+
+    // Let's calculate the difference between the two dates
+    const diffTime = dateThreshold - updatedAt;
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+    if (diffDays > 0) {
+      await octokit.issues.createComment({
+        owner,
+        repo,
+        issue_number: issue.number,
+        body: `This application has not seen any responses in the last 10 days. This issue will be marked with Stale label and will be closed in 4 days. Comment if you want to keep this application open.
 \n\n
 --
 Commented by Stale Bot.`
-            });
-            console.log(`Stale advice on issue ${issue.number}. Updated ${diffDays} days ago`);
-          } else {
-            console.log(`No stale advice on issue ${issue.number}. Updated ${diffDays} days ago`);
-          }
-          return resolve();
-        }
-      )
-    )
-  );
+      });
+      console.log(`Stale advice on issue ${issue.number}. Updated ${diffDays} days ago`);
+    } else {
+      console.log(`No stale advice on issue ${issue.number}. Updated ${diffDays} days ago`);
+    }
+  }
 }
 
 checkAndCommentOnIssues();
--- .github/scripts/close_inactive/utils.mjs
+++ .github/scripts/close_inactive/utils.mjs
@@ -1,3 +1,15 @@
+const RATE_REMAINING_LIMIT = 100;
+
+/**
+ * This function returns the last comment on an issue.
+ * 
+ * @param {Octokit} octokit 
+ * @param {string} owner 
+ * @param {string} repo 
+ * @param {number} issueNumber 
+ * @param {boolean} includeBot 
+ * @returns 
+ */
 export async function getLastComment(octokit, owner, repo, issueNumber, includeBot) {
   const botSignature = "Commented by Stale Bot."
 
@@ -22,4 +34,24 @@ export async function getLastComment(octokit, owner, repo, issueNumber, includeB
   // Order comments by date desc.
   comments.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
   return comments[0];
+}
+
+/**
+ * This function checks the rate limit status and sleeps if the remaining requests are below the limit.
+ * 
+ * @param {Octokit} octokit 
+ */
+export async function checkThrottling(octokit) {
+  const rateLimitStatus = await octokit.rateLimit.get();
+  const remaining = rateLimitStatus.data.rate.remaining;
+
+  if (remaining < RATE_REMAINING_LIMIT) {
+    const timestampNow = new Date().getTime()
+    const timestampReset = rateLimitStatus.data.rate.reset * 1000
+    const sleepTime = (timestampReset - timestampNow)
+
+    const sleepTimeStringInMinutesAndSeconds = new Date(sleepTime).toISOString().substr(14, 5);
+    console.log(`Rate limit reached. Throttling for ${sleepTimeStringInMinutesAndSeconds} minutes`);
+    await new Promise(resolve => setTimeout(resolve, sleepTime));
+  }
 }
