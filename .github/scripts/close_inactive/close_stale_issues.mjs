2f3a640fa8818390ec4a6f390f2531d360f94de5
Charly López <charly@keyko.io>
2023-08-30 16:26:42 +0200

    add error management

--- .github/scripts/close_inactive/close_stale_issues.mjs
+++ .github/scripts/close_inactive/close_stale_issues.mjs
@@ -32,46 +32,50 @@ async function closeStaleIssues() {
   dateThreshold.setDate(dateThreshold.getDate() - DAYS_TO_WAIT);
 
   for (let issue of issues) {
-    await checkThrottling(octokit);
-    
-    const lastComment = await getLastComment(octokit, owner, repo, issue.number, false);
-    let updatedAt = lastComment 
-      ? new Date(lastComment.created_at) 
-      : new Date(issue.created_at);
+    try {
+      await checkThrottling(octokit);
+      
+      const lastComment = await getLastComment(octokit, owner, repo, issue.number, false);
+      let updatedAt = lastComment 
+        ? new Date(lastComment.created_at) 
+        : new Date(issue.created_at);
 
-    // Let's calculate the difference between the two dates
-    const diffTime = dateThreshold - updatedAt;
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+      // Let's calculate the difference between the two dates
+      const diffTime = dateThreshold - updatedAt;
+      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
-    if (diffDays > 0) {
-      await octokit.issues.createComment({
-        owner,
-        repo,
-        issue_number: issue.number,
-        body: `This application has not seen any responses in the last 14 days, so for now it is being closed. Please feel free to contact the Fil+ Gov team to re-open the application if it is still being processed. Thank you!
-\n\n
---
-Commented by Stale Bot.`
-      });
-      // Add stale label.
-      await octokit.issues.addLabels({
-        owner,
-        repo,
-        issue_number: issue.number,
-        labels: ['stale']
-      });
+      if (diffDays > 0) {
+        await octokit.issues.createComment({
+          owner,
+          repo,
+          issue_number: issue.number,
+          body: `This application has not seen any responses in the last 14 days, so for now it is being closed. Please feel free to contact the Fil+ Gov team to re-open the application if it is still being processed. Thank you!
+  \n\n
+  --
+  Commented by Stale Bot.`
+        });
+        // Add stale label.
+        await octokit.issues.addLabels({
+          owner,
+          repo,
+          issue_number: issue.number,
+          labels: ['stale']
+        });
 
-      // Close issue.
-      await octokit.issues.update({
-        owner,
-        repo,
-        issue_number: issue.number,
-        state: 'closed'
-      });
+        // Close issue.
+        await octokit.issues.update({
+          owner,
+          repo,
+          issue_number: issue.number,
+          state: 'closed'
+        });
 
-      console.log(`Let's close issue ${issue.number}. Last commented ${diffDays} days ago`);
-    } else {
-      console.log(`Issue ${issue.number} will remain open. Updated ${diffDays} days ago`);
+        console.log(`Let's close issue ${issue.number}. Last commented ${diffDays} days from threshold`);
+      } else {
+        console.log(`Issue ${issue.number} will remain open. Updated ${diffDays} days from threshold`);
+      }
+    } catch (error) {
+      console.log(`Error in ${issue.number}: ${error.message}`);
     }
   }
 }
